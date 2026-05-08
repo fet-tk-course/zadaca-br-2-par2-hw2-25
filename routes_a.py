a8b17031e085cefff0558fd3128dbf1de28225a7
Dženan <dzenan.cerkezovic@fet.ba>
2026-05-08 21:36:34 +0200

    Refaktorisanje GET endpointa za resurs tereni

--- routes_a.py
+++ routes_a.py
@@ -1,28 +1,25 @@
-from fastapi import APIRouter, Depends, HTTPException, Query
+from fastapi import APIRouter, Depends, HTTPException
 from sqlmodel import Session, select
 from typing import Optional
 from database import get_session
-from models_a import Teren, TerenCreate, TerenUpdate
+from models_a import Teren
 
 
-router = APIRouter(prefix="/tereni", tags=["Tereni"])
+router = APIRouter()
 
 
-@router.get("/", response_model=list[Teren])
-def get_tereni(
-    surface: Optional[str] = Query(default=None, description="Filter po vrsti podloge"),
-    session: Session = Depends(get_session)
-):
+@router.get("/tereni")
+def get_all_tereni(surface: Optional[str] = None,
+                   session: Session = Depends(get_session)):
     query = select(Teren)
     if surface is not None:
         query = query.where(Teren.surface == surface)
-    tereni = session.exec(query).all()
-    return tereni
+    return session.exec(query).all()
 
 
-@router.get("/{teren_id}", response_model=Teren)
-def get_teren(teren_id: int, session: Session = Depends(get_session)):
-    teren = session.get(Teren, teren_id)
+@router.get("/tereni/{id}")
+def get_teren(id: int, session: Session = Depends(get_session)):
+    teren = session.get(Teren, id)
     if not teren:
         raise HTTPException(status_code=404, detail="Teren nije pronađen")
     return teren
