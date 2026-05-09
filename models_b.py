34abe5f1b6c2fc167bf8f35991bb4c4be9c1a5c1
Mahir_Durakovic <mahir.durakovic@fet.ba>
2026-05-09 13:42:55 +0200

    Implementirana Rezervacija SQLModel i pripadajuće Create/Update šeme

--- models_b.py
+++ models_b.py
@@ -1,5 +1,28 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
 
-# TODO: Student B - Definiši svoj SQLModel entitet ovdje
-# 
+class Rezervacija(SQLModel, table=True):
+
+    id: Optional[int] = Field(default=None, primary_key=True)
+    user: str
+    date: str
+    time_begin: str
+    duration_h: int
+    teren_id: int  # veza na Teren model
+    status: Optional[str] = "pending"
+
+class RezervacijaCreate(SQLModel):
+    user: str
+    date: str
+    time_begin: str
+    duration_h: int
+    teren_id: int
+
+
+class RezervacijaUpdate(SQLModel):
+    user:       Optional[str] = None
+    date:       Optional[str] = None
+    time_begin: Optional[str] = None
+    duration_h: Optional[int] = None
+    teren_id:   Optional[int] = None
+    status:     Optional[str] = None
