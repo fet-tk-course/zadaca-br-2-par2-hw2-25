f902fa6b2efe8336ab880e42c50925457dad57b7
Mahir_Durakovic <mahir.durakovic@fet.ba>
2026-05-12 12:46:12 +0200

    Ubacen tip podatka datetime

--- models_b.py
+++ models_b.py
@@ -1,28 +1,29 @@
 from sqlmodel import SQLModel, Field
 from typing import Optional
+from datetime import datetime
 
 class Rezervacija(SQLModel, table=True):
 
     id: Optional[int] = Field(default=None, primary_key=True)
     user: str
-    date: str
-    time_begin: str
+    date: datetime
+    time_begin: datetime
     duration_h: int
-    teren_id: int  # veza na Teren model
+    teren_id: int  
     status: Optional[str] = "pending"
 
 class RezervacijaCreate(SQLModel):
     user: str
-    date: str
-    time_begin: str
+    date: datetime
+    time_begin: datetime
     duration_h: int
     teren_id: int
 
 
 class RezervacijaUpdate(SQLModel):
     user:       Optional[str] = None
-    date:       Optional[str] = None
-    time_begin: Optional[str] = None
+    date:       Optional[datetime] = None
+    time_begin: Optional[datetime] = None
     duration_h: Optional[int] = None
     teren_id:   Optional[int] = None
     status:     Optional[str] = None
